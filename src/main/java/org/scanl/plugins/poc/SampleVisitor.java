dcf576607a3ab865f2be907d81b9f41546ec65a7
ses7578 <ses7578@rit.edu>
2021-10-26 19:14:50 -0400

    Found a nicer way of doing things

--- src/main/java/org/scanl/plugins/poc/SampleVisitor.java
+++ src/main/java/org/scanl/plugins/poc/SampleVisitor.java
@@ -1,11 +1,14 @@
 package org.scanl.plugins.poc;
 
 import com.intellij.psi.*;
+import org.graalvm.compiler.graph.Node;
+import org.jetbrains.annotations.NotNull;
 import org.scanl.plugins.poc.model.Class;
 import org.scanl.plugins.poc.model.Method;
 import org.scanl.plugins.poc.model.SmellType;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -13,34 +16,45 @@ public class SampleVisitor extends JavaRecursiveElementVisitor {
     private final List<Method> psiMethods = new ArrayList<>();
     private final List<Class> psiClasses = new ArrayList<>();
     private final List<SmellType> smellTypes = new ArrayList<>();
-    private boolean issues = false;
 
-    boolean methodIssue = false;
+
 
 
     @Override
     public void visitMethod(PsiMethod method) {
+        boolean methodIssue = false;
+        boolean empty = false;
         Method.MethodType methodType = Method.MethodType.Method;
         if (method.isConstructor())
             methodType = Method.MethodType.Constructor;
-        if(method.getBody().isEmpty())
+        if(method.getBody().isEmpty()) {
+            empty = true;
             smellTypes.add(SmellType.EMPTY_METHOD);
-        if(methodIssue || method.getBody().isEmpty()) {
-            issues = true;
+        }
+        PsiStatement @NotNull [] statements = method.getBody().getStatements();
+        for(PsiStatement statement: statements) {
+            if(statement instanceof PsiExpressionStatement)
+            {
+                PsiExpressionStatement expressionStatement = (PsiExpressionStatement) statement;
+                PsiExpression expression = expressionStatement.getExpression();
+                if(expression instanceof PsiMethodCallExpression) {
+                    PsiMethodCallExpression methodCallExpression = (PsiMethodCallExpression) expression;
+                    if (methodCallExpression.getMethodExpression().getQualifierExpression() != null) {
+                        PsiType s = methodCallExpression.getMethodExpression().getQualifierExpression().getType();
+                        if(s!=null) {
+                            if (hasRedundantIssue(methodCallExpression))
+                                methodIssue = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        if(methodIssue || empty) {
             getPsiMethods().add(new Method(method.getName(), 1, 1, methodType, method, smellTypes));
         }
-        super.visitMethod(method);
-    }
 
-    @Override
-    public void visitMethodCallExpression(PsiMethodCallExpression expression) {
-        super.visitMethodCallExpression(expression);
-        if (expression.getMethodExpression().getQualifierExpression() == null)
-            return;
-        PsiType s = expression.getMethodExpression().getQualifierExpression().getType();
-        if (s == null)
-            return;
-        methodIssue = hasRedundantIssue(expression);
+        super.visitMethod(method);
     }
 
     private boolean hasRedundantIssue(PsiMethodCallExpression expression)
@@ -61,13 +75,5 @@ public class SampleVisitor extends JavaRecursiveElementVisitor {
 
     public List<Class> getPsiClasses() {
         return psiClasses;
-    }
-
-    public List<SmellType> getSmellTypes(){
-        return smellTypes;
-    }
-
-    public boolean hasIssues(){
-        return issues;
     }
 }
